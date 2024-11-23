22a7c534d02117bfcef6604d2ad71f80e7a6b76a
bibblybobblyben <ben.s.mawdsley@hotmail.co.uk>
2024-11-23 17:22:05 +0000

    Make plots for descriptive stats

--- scripts/paper_plots.py
+++ scripts/paper_plots.py
@@ -55,6 +55,89 @@ fig.tight_layout()
 fig.savefig("../figs/ModelComparison.png", dpi=250)
 
 
+#####
+# What do the scores of students typically look like?
+#####
+
+with open("../data/outputs/TotalExamScores.json", "r", encoding="utf-8") as f:
+    tot_scores = json.load(f)[0]
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(7.5, 4))
+labels = []
+means = []
+stds = []
+errs = []
+allraws = []
+allbins = []
+for i in range(len(tot_scores["raw"])):
+    labels.append(f"Exam {i}")
+    _ = [allraws.append(a) for a in tot_scores["raw"][f"Exam_{i}"]]
+    _ = [allbins.append(a) for a in tot_scores["binarised"][f"Exam_{i}"]]
+    means.append(np.mean(tot_scores["binarised"][f"Exam_{i}"]))
+    stds.append(
+        np.std(tot_scores["binarised"][f"Exam_{i}"])
+        # / np.sqrt(len(tot_scores["binarised"][f"Exam_{i}"]))
+    )
+    errs.append(
+        np.std(tot_scores["binarised"][f"Exam_{i}"])
+        / np.sqrt(len(tot_scores["binarised"][f"Exam_{i}"]))
+    )
+
+
+ax[0].errorbar(
+    labels,
+    means,
+    yerr=stds,
+    color="hclightblue",
+    marker="",
+    linestyle=" ",
+    linewidth=0,
+    capsize=8,
+    capthick=0.8,
+    elinewidth=0.5,
+)
+ax[0].scatter(labels, means, marker="o", color="hclightblue")
+ax[0].set_ylim((0, 1))
+ax[0].set_ylabel("Fractional score")
+ax[0].set_title("Score distributions")
+ax[0].tick_params(axis="x", labelrotation=90)
+
+ax[1].errorbar(
+    labels,
+    means,
+    yerr=errs,
+    color="hccoral",
+    marker="",
+    linestyle=" ",
+    linewidth=0,
+    capsize=8,
+    capthick=0.8,
+    elinewidth=0.5,
+)
+ax[1].scatter(labels, means, marker="o", color="hccoral")
+ax[1].tick_params(axis="x", labelrotation=90)
+ax[1].set_ylabel("Fractional score")
+ax[1].set_title("Mean scores")
+fig.tight_layout()
+fig.savefig("../figs/StudentTypicalScores_ByExam.png", dpi=500)
+
+
+bins = np.linspace(0, 1, 20)
+fig, ax = plt.subplots(nrows=1, ncols=1, sharey=True, figsize=(4, 4))
+
+rdat = np.array(allraws).flatten()
+bdat = np.array(allbins).flatten()
+ax.hist(rdat, bins=bins, label="Raw")
+ax.hist(bdat, bins=bins, histtype="step", linewidth=3, label="Binary")
+ax.legend(loc="upper right")
+ax.set_ylabel("Number of exams")
+ax.set_xlabel("Fractional Grade")
+ax.set_xlabel("Fractional Grade")
+
+
+fig.savefig("../figs/DistributionOfExamScores.png", dpi=500)
+
+
 ###
 # View KNN performance as f(n_neighbours)
 ###
